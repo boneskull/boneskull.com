e8ec60861f04446085be8402acb64959815b8709
Christopher Hiller <boneskull@boneskull.com>
2017-09-11 23:08:36 -0700

    reformat custom code

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,44 +1,59 @@
 // Hamburger Menu
-var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
-
-    var hamburgers = document.querySelectorAll(".hamburger");
-    if (hamburgers.length > 0) {
-      forEach(hamburgers, function(hamburger) {
-        hamburger.addEventListener("click", function() {
-          this.classList.toggle("is-active");
-        }, false);
-      });
+var forEach = function (t, o, r) {
+  if ("[object Object]" === Object.prototype.toString.call(t)) {
+    for (var c in
+      t) {
+      Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
     }
+  } else {
+    for (var e = 0, l = t.length; l > e; e++) {
+      o.call(r, t[e], e, t)
+    }
+  }
+};
+
+var hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+  forEach(hamburgers, function (hamburger) {
+    hamburger.addEventListener("click", function () {
+      this.classList.toggle("is-active");
+    }, false);
+  });
+}
 
 // jQuery
-$(document).ready(function(){
-  // Menu
-  $( ".hamburger" ).click(function() {
-  $( ".menu" ).toggle();
-});
+$(document)
+  .ready(function () {
+    // Menu
+    $(".hamburger")
+      .click(function () {
+        $(".menu")
+          .toggle();
+      });
 
-$('.featured-slider').slick({
-  dots: true,
-  arrows: false,
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    ]
-});
+    $('.featured-slider')
+      .slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
 
-}); //jQuery ready
+  }); //jQuery ready
