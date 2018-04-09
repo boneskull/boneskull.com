4fa0b1a1fff619f740d58f797956751e1f21edaa
Christopher Hiller <boneskull@boneskull.com>
2018-04-09 11:06:25 -0700

    add some partials, reformat, autoload prism languages, add plugins
    - footer
    - scripts
    - styles
    - ditch GA
    - reformat
    - defer more code loading
    - defer prism highlighting
    - avoid issues with anchor not working
    - simplify hamburger code, though I don't think I even use it.
    
    Signed-off-by: Christopher Hiller <boneskull@boneskull.com>

---
+++ src/assets/js/anchor.js
@@ -0,0 +1,4 @@
+anchors.options = {
+  placement: 'left'
+};
+anchors.add('h2, h3, h4');
--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,62 +1,39 @@
-// Hamburger Menu
-var forEach = function (t, o, r) {
-  if ("[object Object]" === Object.prototype.toString.call(t)) {
-    for (var c in
-      t) {
-      Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
-    }
-  } else {
-    for (var e = 0, l = t.length; l > e; e++) {
-      o.call(r, t[e], e, t)
-    }
-  }
-};
+Prism.plugins.autoloader.languages_path =
+  '//cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/components/';
 
-var hamburgers = document.querySelectorAll(".hamburger");
-if (hamburgers.length > 0) {
-  forEach(hamburgers, function (hamburger) {
-    hamburger.addEventListener("click", function () {
-      this.classList.toggle("is-active");
-    }, false);
+$(document).ready(function() {
+  Prism.highlightAll();
+  // Menu
+  $('.hamburger').click(function() {
+    $('.menu').toggle();
+    $(this).toggleClass('is-active');
   });
-}
 
-// jQuery
-$(document)
-  .ready(function () {
-    // Menu
-    $(".hamburger")
-      .click(function () {
-        $(".menu")
-          .toggle();
-      });
+  $('.featured-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
-    $('.featured-slider')
-      .slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
-
-      $('.content .post-article img').each(function () {
-        $(this).featherlight($(this).attr('src'));
-      });
-  }); //jQuery ready
+  $('.content .post-article img').each(function() {
+    $(this).featherlight($(this).attr('src'));
+  });
+}); //jQuery ready
